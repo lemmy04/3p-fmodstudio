011b61413b5ea77e99900afe4484a655f4b1fb84
Ansariel <ansariel.hiller@phoenixviewer.com>
2023-01-18 19:22:26 +0100

    Update to 2.02.11

--- build-cmd.sh
+++ build-cmd.sh
@@ -2,8 +2,8 @@
 
 FMOD_DOWNLOAD_BASE="http://erebus/dev/pkg/"
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20209"
-FMOD_VERSION_PRETTY="2.02.09"
+FMOD_VERSION="20211"
+FMOD_VERSION_PRETTY="2.02.11"
 
 cd "$(dirname "$0")"
 
